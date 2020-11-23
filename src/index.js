01ff79f40077e1ad25ec2753129f07f0bcf1d2e2
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-23 20:29:44 +0100

    Extract routes from index.js

--- src/index.js
+++ src/index.js
@@ -3,25 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const constants = require('./config/constants');
 const middlewareConfig = require('./middleware');
+const apiEndpoints = require('./endpoints');
 
 const app = express();
 middlewareConfig(app);
-
-// GET /
-app.get('/', (req, res) => {
-    res.send('Hello, world!'); // default: 200, text/html
-});
-
-// curl -i --data '{"day":"monday2"}' -H 'Content-Type: application/json' -XPOST http://localhost:3001/
-app.post('/', (req, res) => {
-    console.log(JSON.stringify(req.body, null, 2));
-    res.json({ ok: true });
-});
-
-app.get('/ping', (req, res) => {
-    //res.json({ status: 'ok' });
-    res.status(201).json({ status: 'ok' });
-});
+apiEndpoints(app);
 
 const PORT = constants.PORT;
 app.listen(PORT, (err) => {
